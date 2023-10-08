41b88986b0b4528cb034eb39c7aaebf8221e81e5
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2023-10-08 07:37:40 -0300

    ExMedia

---
+++ Media.py
@@ -0,0 +1,9 @@
+n1= float (input (' Digite uma nota :'))
+n2= float (input (' Digite outra nota :'))
+
+print ('A primeira nota foi : {} '.format (n1))
+print ('A segunda nota foi : {}'.format(n2))
+
+media = (n1+n2)/2
+
+print ('A média {}'.format (media))

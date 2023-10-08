6d487b1589d96b1a91ac018d875a1f4f59649419
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2023-10-08 08:31:33 -0300

    Update Tabuada.py

--- Tabuada.py
+++ Tabuada.py
@@ -16,3 +16,17 @@ print ('A tabuada do {} fica assim : ' .format(n))
 
 print (n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11)
 
+print (8*'---')
+
+print ('{} x {}  = {}'.format(n,1, n*1))
+print ('{} x {}  = {}'.format(n,2, n*2))
+print ('{} x {}  = {}'.format(n,3, n*3))
+print ('{} x {}  = {}'.format(n,4, n*4))
+print ('{} x {}  = {}'.format(n,5, n*5))
+print ('{} x {}  = {}'.format(n,6, n*6))
+print ('{} x {}  = {}'.format(n,7, n*7))
+print ('{} x {}  = {}'.format(n,8, n*8))
+print ('{} x {}  = {}'.format(n,9, n*9))
+print ('{} x {} = {}'.format(n,10, n*10))
+print (8*'-----')
+

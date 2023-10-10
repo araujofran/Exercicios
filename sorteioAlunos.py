dacfc4d0c4df9261054c47ea56d84cc9e25374c4
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2023-10-09 21:50:26 -0300

    Aumentando o Conhecimento

--- sorteioAlunos.py
+++ sorteioAlunos.py
@@ -14,4 +14,16 @@ elif num == 2:
 else :
     print ('Fábio')
 
-    
+print (8*"*********")
+
+n1= str(input ('Digite o nome do primeiro aluno : '))
+n2= str(input ('Digite o nome do segundo aluno: '))
+n3= str(input ('Digite o nome do terceiro aluno: '))
+n4= str(input ('Digite o nome do quarto aluno: '))
+
+lista = [n1,n2,n3,n4]
+
+escolhido = random.choice (lista)
+
+print ('O nome escolhido foi {}'.format(escolhido))
+

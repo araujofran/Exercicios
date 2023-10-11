b840e7b1824cec06156be348eabcc5089f5f2243
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2023-10-10 21:07:53 -0300

    Correção

--- Desafio03.py
+++ Desafio03.py
@@ -7,4 +7,9 @@ cidade.find('Santo') # Santo esta na posição zero?
 print ('A cidade começa com a palavra Santo? {}'. format(cidade.find('Santo')==(0)))# Santo esta na posição zero?
 
 # seu primeiro nome tem 
-print (cidade.find (' '))
+
+separa = cidade.split()
+print(separa)
+
+print ('A primeira palavra da cidade é {} '.format(separa[0]))
+print ('A quantidade de letras nessa expressão é {} '. format(cidade.find (' ')))
--- Desafio06.py
+++ Desafio06.py
@@ -1,1 +1,9 @@
 # ler Nome e Sobrenome
+
+nome = str(input ('Digite o nome e o seu sobrenome : ')).strip()
+separa = nome.split()
+separasobrenome = nome.rsplit()
+print(separasobrenome)
+
+print ('O seu nome é {} e o seu sobrenome é {}.'.format(separa[0],separasobrenome[1]))
+print ('A quantidade de letras que contem seu nome são {} .'.format(nome.find(' ')))

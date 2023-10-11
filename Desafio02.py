c5334b6336d01681f79348097a4ef8edd1effa35
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2023-10-11 18:21:17 -0300

    Simplicidades é tudo

--- Desafio02.py
+++ Desafio02.py
@@ -1,5 +1,11 @@
 import random
 
 num = int (input ('Digite um número com 4 algarismos: '))
-print ( ' O seu número digitado foi : {:.0f}'.format(num))
-print (num.split()))
+
+n= str(num)
+
+
+print ('Seu número digitado tem milhar: {}'.format(n[0]))
+print ('Seu número digitado tem centena: {}'.format(n[1]))
+print ('Seu número digitado tem dezena: {}'.format(n[2]))
+print ('Seu número digitado tem unidade: {}'.format(n[3]))

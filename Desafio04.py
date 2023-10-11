5585af20a1ed976221cb5fa419510cfb956356ba
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2023-10-11 18:48:19 -0300

    Nota dez

--- Desafio04.py
+++ Desafio04.py
@@ -1,4 +1,8 @@
 nome = (input ('Digite um  nome : '))
 
+nome = nome.upper()
+
 print (" O nome {} contem a palavra Silva?".format(nome))
-print ('Silva' in nome)
+print ('SILVA' in nome)
+
+print (" O nome {} contem a palavra Silva?{}".format(nome,'SILVA' in nome))

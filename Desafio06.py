b7a232bb6b50e2febaf7a0df71637c3915258d9b
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2023-10-11 19:29:12 -0300

    Ufa Acabou Aula 9

--- Desafio06.py
+++ Desafio06.py
@@ -1,10 +1,6 @@
 # ler Nome e Sobrenome
 
-nome = str(input ('Digite o nome e o seu sobrenome : ')).strip()
-separa = nome.split()
-separasobrenome = nome.rsplit()
-print(separasobrenome)
+n = str(input ('Digite o nome e o seu sobrenome : ')).strip()
+nome = n.split()
 
-print ('O seu nome é {} e o seu sobrenome é {}.'.format(separa[0],separasobrenome[2]))
-print ('A quantidade de letras que contem seu nome são {} .'
-       .format(nome.find(' ')))
+print ('O seu nome é {} e o seu ultimo nome é {}.'.format(nome[0], nome[len(nome)-1]))
